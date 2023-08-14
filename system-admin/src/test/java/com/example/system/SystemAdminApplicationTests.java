1005e7b9844fe723377355e2b92cb3b996633445
unknown <2907520924@qq.com>
2023-08-14 17:02:38 +0800

    数据

--- system-admin/src/test/java/com/example/system/SystemAdminApplicationTests.java
+++ system-admin/src/test/java/com/example/system/SystemAdminApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class SystemAdminApplicationTests {
 
 	@Test
 	void contextLoads() {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		System.out.println("加密后"+passwordEncoder.encode("123456"));
 	}
 
 }
